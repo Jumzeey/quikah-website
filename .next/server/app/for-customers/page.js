2b161a47907c82a3ce0af8596ff4c222ee0cd7bb
Jumzeey <31732947+Jumzeey@users.noreply.github.com>
2025-08-20 02:58:08 +0100

    Remove .next folder from Git tracking - build files should not be committed

--- .next/server/app/for-customers/page.js
+++
@@ -1,6 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/for-customers/page.js";
-const runtime = require("../../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/[root of the server]__c918e6._.js");
-runtime.loadChunk("server/chunks/ssr/node_modules_fd5cd5._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/for-customers/page/actions.js [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-page.js?page=/for-customers/page { COMPONENT_0 => \"[project]/app/layout.tsx [app-rsc] (ecmascript, Next.js server component)\", COMPONENT_1 => \"[project]/node_modules/next/dist/client/components/not-found-error.js [app-rsc] (ecmascript, Next.js server component)\", COMPONENT_2 => \"[project]/app/for-customers/page.tsx [app-rsc] (ecmascript, Next.js server component)\" } [app-rsc] (ecmascript) <facade>", CHUNK_PUBLIC_PATH).exports;
